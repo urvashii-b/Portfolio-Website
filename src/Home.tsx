e70d804a67a91575d5c4c21c573c776f1c0031cf
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-16 22:06:50 +0530

    imported images

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,24 @@ import Sidebar from './components/NavBar/Sidebar';
 import './index.css'
 
 import img1 from './assets/images/cpp.png';
+import img2 from './assets/images/framer.png';
+import img3 from './assets/images/gpt.png';
+import img4 from './assets/images/mongo.png';
+import img5 from './assets/images/mysql.png';
+import img6 from './assets/images/py.png';
+import img7 from './assets/images/react.png';
+import img8 from './assets/images/tailwind.png';
+import img9 from './assets/images/vite.png';
+
+import p1 from './assets/images/mediwise.png';
+import p2 from './assets/images/emotify.png';
+import p3 from './assets/images/llmhub.png';
+import p4 from './assets/images/aibotique.png';
+import p5 from './assets/screenshare.mp4';
+import p6 from './assets/images/guide.png';
+import p7 from './assets/images/findIssue.png';
+import p8 from './assets/n-queens.mp4';
+import p9 from './assets/images/da-hackathon.png';
 
 const Home: React.FC = () => {
     return (
@@ -23,7 +41,7 @@ const Home: React.FC = () => {
             <h2>Projects</h2>
             <div className="projects-grid">
               <div className="project-card">
-                <img src="src\assets\images\mediwise.png" alt="Mediwise" width={400} height={250}/>
+                <img src={p1} alt="Mediwise" width={400} height={250}/>
                 <div className="project-details">
                   <div className="project-title">Mediwise ~ Winner | Ingenius 12.0</div>
                   <p className='p1'>Smart Inventory Management</p>
@@ -31,7 +49,7 @@ const Home: React.FC = () => {
               </div>
               
               <div className="project-card">
-                <img src="src\assets\images\emotify.png" alt="Emotify" width={380} height={350}/>
+                <img src={p2} alt="Emotify" width={380} height={350}/>
                 <div className="project-details">
                   <div className="project-title">Emotify | Arithemania 2.0</div>
                   <p className='p1'>Music Recommendation System ↗</p>
@@ -39,7 +57,7 @@ const Home: React.FC = () => {
               </div>
   
               <div className="project-card">
-                <img src="src\assets\images\llmhub.png" alt="LLM-Hub" width={400} height={200}/>
+                <img src={p3} alt="LLM-Hub" width={400} height={200}/>
                 <div className="project-details">
                   <div className="project-title">LLM-Hub</div>
                   <p className='p1'>Repository of Generative AI Projects ↗</p>
@@ -48,14 +66,14 @@ const Home: React.FC = () => {
   
               {/* Stack 2 */}
               <div className="project-card">
-                <img src="src\assets\images\aibotique.png" alt="AiBotique" width={380} height={350}/>
+                <img src={p4} alt="AiBotique" width={380} height={350}/>
                 <div className="project-details">
                   <div className="project-title">AiBotique ~ Top 10 | Kodikon 3.0</div>
                   <p className='p1'>DALL·E 2 for generating styles ↗</p>
                 </div>
               </div>
               <div className="project-card">
-                <video src="src\assets\screenshare.mp4" loop muted autoPlay playsInline width={400}></video>
+                <video src={p5} loop muted autoPlay playsInline width={400}></video>
                 <div className="project-details">
                   <div className="project-title">Screen Share</div>
                   <p className='p1'>View others screens on your own ↗</p>
@@ -63,7 +81,7 @@ const Home: React.FC = () => {
               </div>
               
               <div className="project-card">
-                <img src="src\assets\images\guide.png" alt="Guide" width={400}/>
+                <img src={p6} alt="Guide" width={400}/>
                 <div className="project-details">
                   <div className="project-title">Guide ~ SIH 2023</div>
                   <p className='p1'>Future help with AI prototype</p>
@@ -71,21 +89,21 @@ const Home: React.FC = () => {
               </div>
               {/* Stack 3 */}
               <div className="project-card">
-                <img src="src\assets\images\findissue.png" alt="findIssue" width={400} height={600}/>
+                <img src={p7} alt="findIssue" width={400} height={600}/>
                 <div className="project-details">
                   <div className="project-title">findIssue ~ Buildspace</div>
                   <p className='p1'>Open-Source Contributing Assistant ↗</p>
                 </div>
               </div>
               <div className="project-card">
-                <img src="src\assets\images\da-hackathon.png" alt="Project 8" height={550} width={390}/>
+                <img src={p9} alt="Project 8" height={550} width={390}/>
                 <div className="project-details">
                   <div className="project-title">Kaggle Rank: 32/300</div>
                   <p className='p1'>Stock Price Prediction ↗</p>
                 </div>
               </div>
               <div className="project-card">
-                <video src="src\assets\n-queens.mp4" loop muted autoPlay playsInline width={400}></video>
+                <video src={p8} loop muted autoPlay playsInline width={400}></video>
                 <div className="project-details">
                   <div className="project-title">Royal Resolve</div>
                   <p className='p1'>N-Queens Python Game ↗</p>
@@ -100,24 +118,14 @@ const Home: React.FC = () => {
             <div className="tech-stack-logos-container">
             <div className="tech-stack-logos">
               <img src={img1} alt="C++ Logo" className="tech-logo" />
-              <img src="src\assets\images\framer.png" alt="Framer Logo" className="tech-logo" />
-              <img src="src\assets\images\gpt.png" alt="GPT Logo" className="tech-logo" />
-              <img src="src\assets\images\mysql.png" alt="MySQL Logo" className="tech-logo" />
-              <img src="src\assets\images\py.png" alt="Python Logo" className="tech-logo" />
-              <img src="src\assets\images\tailwind.png" alt="Tailwind CSS Logo" className="tech-logo" />
-              <img src="src\assets\images\vite.png" alt="Vite Logo" className="tech-logo" />
-              <img src="src\assets\images\react.png" alt="React Logo" className="tech-logo" />
-              <img src="src\assets\images\mongo.png" alt="Mongo Logo" className="tech-logo" />
-              <img src="src\assets\images\cpp.png" alt="C++ Logo" className="tech-logo" />
-              <img src="src\assets\images\framer.png" alt="Framer Logo" className="tech-logo" />
-              <img src="src\assets\images\gpt.png" alt="GPT Logo" className="tech-logo" />
-              <img src="src\assets\images\mysql.png" alt="MySQL Logo" className="tech-logo" />
-              <img src="src\assets\images\py.png" alt="Python Logo" className="tech-logo" />
-              <img src="src\assets\images\tailwind.png" alt="Tailwind CSS Logo" className="tech-logo" />
-              <img src="src\assets\images\vite.png" alt="Vite Logo" className="tech-logo" />
-              <img src="src\assets\images\react.png" alt="React Logo" className="tech-logo" />
-              <img src="src\assets\images\mongo.png" alt="Mongo Logo" className="tech-logo" />
-              
+              <img src={img2} alt="Framer Logo" className="tech-logo" />
+              <img src={img3} alt="GPT Logo" className="tech-logo" />
+              <img src={img5} alt="MySQL Logo" className="tech-logo" />
+              <img src={img6} alt="Python Logo" className="tech-logo" />
+              <img src={img8} alt="Tailwind CSS Logo" className="tech-logo" />
+              <img src={img9} alt="Vite Logo" className="tech-logo" />
+              <img src={img7} alt="React Logo" className="tech-logo" />
+              <img src={img4} alt="Mongo Logo" className="tech-logo" /> 
             </div>
           </div>
           <br></br>
