3bab704b912356693da278df91b4c45d53fca7d4
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-07-25 12:54:38 +0530

    about website added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import About from './About';
 import Blogs from './Blog';
 import './App.css';
 
@@ -9,7 +8,6 @@ const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
       <Route path="/blogs" element={<Blogs />} />
     </Routes>
   );
